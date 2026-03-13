82bbcd584b24c1cde4a730c932f410f13dda908f
satheeshpoluab <satheeshpoluab@gmail.com>
2026-03-13 22:23:49 +0100

    test fix

--- src/pages/tests/Contact.test.tsx
+++ src/pages/tests/Contact.test.tsx
@@ -1,6 +1,26 @@
 import { render, screen } from '../../utils/test-utils';
 import Contact from '../Contact';
 
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => {
+      const translations: Record<string, string> = {
+        'contact.title': 'Get in Touch',
+        'contact.description':
+          "We'd love to hear from you. Fill out the form below and we'll get back to you soon!",
+        'contact.form.title': 'Contact details',
+        'contact.form.description': 'Please provide your contact details below.',
+        'contact.form.name': 'Name',
+        'contact.form.email': 'Email',
+        'contact.form.country': 'Country',
+        'contact.form.action': 'Submit',
+      };
+      return translations[key] ?? key;
+    },
+    i18n: { changeLanguage: jest.fn() },
+  }),
+}));
+
 describe('Contact component', () => {
   beforeEach(() => {
     render(<Contact />);
